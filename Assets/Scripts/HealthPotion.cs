09a513517ee901c7b6fb2a7c5a0243da11774c4a
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2023-10-22 22:53:39 +1100

    Updated Health Potion Script
    - Made it so health potions are attracted towards the player when within a certain distance.
    - Made it so health potions give the player 1-20HP and are destroyed on collision with the player.

--- Assets/Scripts/HealthPotion.cs
+++ Assets/Scripts/HealthPotion.cs
@@ -4,17 +4,55 @@ using UnityEngine;
 
 public class HealthPotion : MonoBehaviour
 {
-    public Transform playerTransform;
-    
+    public float attractDistance = 3.5f;
+    public float attractSpeed = 6f;
+    public float idleSlow = 0.1f;
+
+    private Rigidbody2D rb;
+    private Transform playerTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        playerTransform = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log(Vector2.Distance(playerTransform.position, transform.position));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Vector2.Distance(playerTransform.position, transform.position) < attractDistance)
+        {
+            Vector2 targetDirection = (playerTransform.position - transform.position).normalized;
+            rb.velocity = new Vector2(targetDirection.x, targetDirection.y) * attractSpeed;
+        }
+        else if (Vector2.Distance(playerTransform.position, transform.position) > attractDistance)
+        {
+            rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, idleSlow);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController playerController = collision.GetComponent<PlayerController>();
+            playerController.UpdateHealth(Random.Range(1, 20));
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, attractDistance);
     }
 }
