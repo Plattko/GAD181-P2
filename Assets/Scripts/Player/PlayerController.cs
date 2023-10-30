6f4af0e380a99eba6741aadb2c5b7f4ca12be92f
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2023-10-30 21:16:59 +1100

    Added Player Attack Animations
    - Created first attack animations for each direction (up, down, left, right).
    - Created the Attack blend tree in the Player's animator to control which attack animation should be played.
    - Updated the Update method in the PlayerController script to track the player's mouse position.
    - Updated the OnAttack method in the PlayerController script to no longer trigger the Attack animation for the Weapon animator, and instead trigger the Attack animation in the Player animator, attacking in the direction of the mouse from the Player.
    - Disabled the Weapon child game object on the Player.
    
    Other Housekeeping:
    - Organised the variables at the top of the PlayerController script.
    - Organised the Update method in the PlayerController script.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,31 +6,40 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    // Get reference variables
     private Rigidbody2D rb;
     private Animator animator;
+    private Camera mainCamera;
     public HealthBar healthBar;
 
+    // Movement variables
+    private Vector2 moveInput;
+    private float idleSlow = 0.9f;
     public float startingMoveSpeed = 6f;
     [HideInInspector] public float moveSpeed;
+    
+    // Attack variables
     public float startingAttackDamage = 10f;
     [HideInInspector] public float attackDamage;
+
     private float attackCooldown = 0.3f;
-
-    private float idleSlow = 0.9f;
-
-    private Vector2 moveInput;
-
     private bool canAttack = true;
-    public bool isInShop { private get; set; } = false;
 
+    private Vector2 mousePos;
+    private Vector2 direction;
+
+    // Health variables
     public int startingHealth = 100;
     public int currentHealth;
+
+    public bool isInShop { private get; set; } = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        mainCamera = Camera.main;
 
         currentHealth = startingHealth;
         healthBar.SetHealth(currentHealth, startingHealth);
@@ -42,14 +51,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            UpdateHealth(-20);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            UpdateHealth(10);
-        }
+        // Get mouse position
+        mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
         // Update animator
         bool isMoving = moveInput != Vector2.zero;
@@ -63,6 +66,16 @@ public class PlayerController : MonoBehaviour
         else
         {
             animator.SetFloat("Speed", 0f);
+        }
+
+        // Debugging
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UpdateHealth(-20);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            UpdateHealth(10);
         }
     }
 
@@ -110,10 +123,21 @@ public class PlayerController : MonoBehaviour
     {
         if (!isInShop)
         {
+            //if (canAttack)
+            //{
+            //    Weapon weapon = GetComponentInChildren<Weapon>();
+            //    weapon.animator.SetTrigger("Attack");
+
+            //    canAttack = false;
+            //    StartCoroutine(AttackCooldown());
+            //}
+
             if (canAttack)
             {
-                Weapon weapon = GetComponentInChildren<Weapon>();
-                weapon.animator.SetTrigger("Attack");
+                Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
+                animator.SetFloat("AttackHorizontal", direction.x);
+                animator.SetFloat("AttackVertical", direction.y);
+                animator.SetTrigger("Attack");
 
                 canAttack = false;
                 StartCoroutine(AttackCooldown());
