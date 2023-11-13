bdd18bf1362d64ad1f841c0696150ca6cd9f953f
Clay M - 102 Centre Back <140664778+Plattko@users.noreply.github.com>
2023-11-14 09:57:49 +1100

    Implemented Dialogue
    Shop script:
    - Updated shop script to open Witch UI instead of Shop UI, and close both the Shop UI and Witch UI.
    
    Dialogue script:
    - Added dialogueBox, witchUI, and shopUI references.
    - Added dialogueInstance variable.
    - Updated OnEnable to set dialogueFinished to false, increment the dialogueInstance, and enable the dialogue box.
    - Updated the Update method to increment the textIndex if the dialogue is not finished and the player presses left click.
    - Updated the Update method to update the dialogue text based on the dialogueInstance and textIndex, and open the shop and close the dialogue box at the end of the dialogue.

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -6,7 +6,11 @@ using TMPro;
 public class Dialogue : MonoBehaviour
 {
     public TextMeshProUGUI dialogueText;
+    public GameObject dialogueBox;
+    public GameObject witchUI;
+    public GameObject shopUI;
 
+    private int dialogueInstance = 0;
     private int textIndex = 0;
     private bool dialogueFinished;
     
@@ -18,28 +22,79 @@ public class Dialogue : MonoBehaviour
 
     private void OnEnable()
     {
+        //StartCoroutine("Ah, a new traveller...", 5f);
+
+        dialogueFinished = false;
+        dialogueInstance += 1;
+        Debug.Log("Dialogue instance is: " + dialogueInstance);
         textIndex = 0;
+        dialogueBox.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    IEnumerator TypeWriter(string sentence, float time)
-    {
-        string currentText = "";
-        dialogueFinished = false;
-        List<char> charArray = sentence.ToCharArray();
-
-        foreach (char letter in charArray)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !dialogueFinished)
         {
-            currentText += letter;
-            dialogueText.text = currentText;
-            yield return new WaitForSeconds(time);
+            textIndex += 1;
+            Debug.Log("Text index is: " + textIndex);
         }
-
-        dialogueFinished = true;
+        
+        if (dialogueInstance == 1)
+        {
+            if (textIndex == 0)
+            {
+                dialogueText.text = "Ah, a new traveller...";
+            }
+            else if (textIndex == 1)
+            {
+                dialogueText.text = "You've seen the dangers of the forest, I presume.";
+            }
+            else if (textIndex == 2)
+            {
+                dialogueText.text = "I can make you stronger, for a small price.";
+            }
+            else if (textIndex == 3)
+            {
+                dialogueText.text = "Allow me a small portion of your life force... and I shall weave new powers for you to control.";
+            }
+            else if(textIndex == 4)
+            {
+                dialogueFinished = true;
+                dialogueBox.SetActive(false);
+                shopUI.SetActive(true);
+                dialogueText.text = "Welcome back, traveler...";
+            }
+        }
+        else if (dialogueInstance >= 2)
+        {
+            //if (textIndex == 0)
+            //{
+            //    dialogueText.text = "Welcome back, traveler...";
+            //}
+            if (textIndex >= 1)
+            {
+                dialogueFinished = true;
+                dialogueBox.SetActive(false);
+                shopUI.SetActive(true);
+            }
+        }
     }
+
+    //IEnumerator TypeWriter(string sentence, float time)
+    //{
+    //    dialogueFinished = false;
+
+    //    string currentText = "";
+    //    List<char> chars = new List<char>(sentence);
+
+    //    foreach (char letter in chars)
+    //    {
+    //        currentText += letter;
+    //        dialogueText.text = currentText;
+    //        yield return new WaitForSeconds(time);
+    //    }
+
+    //    dialogueFinished = true;
+    //}
 }
